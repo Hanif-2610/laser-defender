a6e174d2d6cb202ff234dbea20a2fbc85c30dc56
Hanif Kukuh Raharjo <hraharjo27@gmail.com>
2024-03-10 19:37:13 +0800

    Add Instantiating Enemies
    - Add Instantiating Enemies feature so multiple enemy could appear at the same time (wave)

--- Laser Defender/Assets/Scripts/Pathfinder.cs
+++ Laser Defender/Assets/Scripts/Pathfinder.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    [SerializeField] WaveConfigSO waveConfig;
+    EnemySpawner enemySpawner;
+    WaveConfigSO waveConfig;
     List<Transform> waypoints;
     int waypointIndex = 0;
     
+    void Awake() 
+    {
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+
     void Start()
     {
+        waveConfig = enemySpawner.GetCurrentWave();
         waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].position;
     }
--- Laser Defender/Assets/Scripts/WaveConfigSO.cs
+++ Laser Defender/Assets/Scripts/WaveConfigSO.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
 
     public Transform GetStartingWaypoint()
     {
