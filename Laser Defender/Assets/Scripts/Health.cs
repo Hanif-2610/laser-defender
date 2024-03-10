e822c5f508f8c965ce6525b5dc3d64fc82771255
Hanif Kukuh Raharjo <hraharjo27@gmail.com>
2024-03-11 07:07:51 +0800

    Add Damage System
    - Add Health and Damage to the Enemy and the player
    - Set up the basic necessity for the projectile system

---
+++ Laser Defender/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        DamageDealer damageDealer = other.GetComponent<DamageDealer>();    
+
+        if(damageDealer != null)
+        {
+            TakeDamage(damageDealer.GetDamage());
+            damageDealer.Hit();
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
