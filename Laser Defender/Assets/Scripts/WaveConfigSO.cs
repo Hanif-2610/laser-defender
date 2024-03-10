547531ed99bef4fd39088af4423a488658431afe
Hanif Kukuh Raharjo <hraharjo27@gmail.com>
2024-03-10 20:56:41 +0800

    Add Coroutines and Loops
    - Add delay for enemy spawn time with Coroutines
    - Add loop system for enemy waves

--- Laser Defender/Assets/Scripts/WaveConfigSO.cs
+++ Laser Defender/Assets/Scripts/WaveConfigSO.cs
@@ -8,6 +8,9 @@ public class WaveConfigSO : ScriptableObject
     [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance = 0f;
+    [SerializeField] float minimumSpawnTime = 0.2f;
 
     public int GetEnemyCount()
     {
@@ -37,5 +40,12 @@ public class WaveConfigSO : ScriptableObject
     public float GetMoveSpeed()
     {
         return moveSpeed;
+    }
+
+    public float GetRandomSpawnTime()
+    {
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance,
+                                        timeBetweenEnemySpawns + spawnTimeVariance);
+        return Mathf.Clamp(spawnTime, minimumSpawnTime, float.MaxValue);
     }
 }
